ec5cdf00144e69b24b2ba16283cb0dcaa40d2b58
qiaokun <qkbeyond@gmail.com>
2019-04-23 17:39:40 +0800

    修改侧边栏为手风琴模式
    :unique-opened="true"

--- src/views/layout/components/Sidebar/index.vue
+++ src/views/layout/components/Sidebar/index.vue
@@ -3,6 +3,7 @@
     <el-menu
       :default-active="$route.path"
       :collapse="isCollapse"
+      :unique-opened="true"
       :background-color="variables.menuBg"
       :text-color="variables.menuText"
       :active-text-color="variables.menuActiveText"
